32eb19dda60002fa0a1ed1eedbdf810d67a22405
Hiromu Asahina <asahina@sasase.ics.keio.ac.jp>
2019-08-15 18:03:13 +0900

    Added functions to get reviewers' profile pages. #18

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -2,3 +2,4 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     console.log(msg);//送られたメッセージをキャッチ
     sendResponse('How R U?');//sendResponseでmsgを送ったスクリプト側にレスを返せる
 });
+
