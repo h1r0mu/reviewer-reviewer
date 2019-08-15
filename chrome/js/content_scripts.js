1656f363a9521dcc572610676afa8a954a1d8eeb
Hiromu Asahina <asahina@sasase.ics.keio.ac.jp>
2019-08-15 21:53:34 +0900

    Added functions to get reviewers' reviews URLs. #19

--- chrome/js/content_scripts.js
+++ chrome/js/content_scripts.js
@@ -1,6 +1,6 @@
-function genPosNegReviewURLs(productPageURL) {
-    let domain = productPageURL.split('/')[2];
-    let product = productPageURL.match(/([A-Z]|[0-9]){10}/)[0];
+function genPosNegReviewUrls(productPageUrl) {
+    let domain = productPageUrl.split('/')[2];
+    let product = productPageUrl.match(/([A-Z]|[0-9]){10}/)[0];
 
     return {
         'pos': `https://${domain}/product-reviews/${product}/` +
@@ -10,8 +10,8 @@ function genPosNegReviewURLs(productPageURL) {
     }
 }
 
-function getDocument(reviewURL) {
-    return axios.get(reviewURL)
+function getDocument(reviewUrl) {
+    return axios.get(reviewUrl)
         .then(response => {
             let parser = new DOMParser();
             return parser.parseFromString(response.data, "text/html");
@@ -21,16 +21,43 @@ function getDocument(reviewURL) {
         })
 }
 
-function findProfileURLs(reviewDocument) {
-    let profiles = reviewDocument.getElementsByClassName("a-profile");
-    return Object.values(profiles).map(profile => profile.href);
+function findUserUrls(reviewDocument) {
+    let users = reviewDocument.getElementsByClassName("a-profile");
+    return Object.values(users).map(user => user.href);
 }
 
-let reviewURLs = genPosNegReviewURLs(location.href)
-for (let reviewURL of Object.values(reviewURLs)) {
-    getDocument(reviewURL)
-        .then(findProfileURLs)
-        .then(profileURLs => {
-            console.log(profileURLs);
-        });
+function findUserReviewUrls(userUrl) {
+    return new Promise((resolve, reject) => {
+        let iframe = document.createElement("iframe");
+        iframe.src = userUrl;
+        iframe.hidden = true;
+        iframe.onload = () => {
+            let userReviews = iframe
+                .contentDocument
+                .getElementsByClassName('a-link-normal profile-at-review-link a-text-normal');
+            resolve(Object.values(userReviews).map(userReview => userReview.href));
+            document.body.removeChild(iframe);
+        };
+        document.body.appendChild(iframe);
+    })
+}
+
+function findUsersReviewUrls(userUrls) {
+    let usersReviewUrls = {};
+    for (let url of userUrls) {
+        findUserReviewUrls(url)
+            .then(reviewUrls => {
+                usersReviewUrls[url] = {'urls': reviewUrls};
+                });
+        break;
+    }
+    return usersReviewUrls
+}
+
+let reviewUrls = genPosNegReviewUrls(location.href)
+for (let reviewUrl of Object.values(reviewUrls)) {
+    getDocument(reviewUrl)
+        .then(findUserUrls)
+        .then(findUsersReviewUrls)
+        .then(usersReviewUrls => console.log(usersReviewUrls));
 }
