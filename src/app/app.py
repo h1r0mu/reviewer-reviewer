33a1ff800bab34a8467491a735a8c7d16dff6796
Hiromu Asahina <asahina@sasase.ics.keio.ac.jp>
2019-08-17 02:33:26 +0900

    Added __init__ and moved pipfiles. #5
    - adding API endpoint. #5

--- src/app/app.py
+++ src/app/app.py
@@ -1,12 +1,32 @@
 import os
-from flask import Flask
+from flask import (
+    Flask,
+    abort,
+    request,
+)
+from werkzeug.wrappers import Response
+
+from app.client import APIClient
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class APIResponse(Response):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.headers['content-type'] = 'application/json'
+
+@app.route('/users/<string:user_id>/reviewers/<string:reviewer_id>', method=('POST',))
+def handle_request(user_id, reviewer_id):
+    client = APIClient()
+    data = request.get_json()
+    profile = client.get_profile(data['reviews'])
+    user = User(user_id)
+    user.profile
+    similarity = calc_similarity(profile)
+    response = Response(content_type='application/json')
+    return 'OK'
 
 
 if __name__ == '__main__':
