54583bb17c46d5b60dfe405c03e2487527bb83fe
Hiromu Asahina <asahina@sasase.ics.keio.ac.jp>
2019-08-17 23:09:44 +0900

    Fixed app response to use jsonify and move it to inside the create_app method. #5

--- src/app/app.py
+++ src/app/app.py
@@ -1,31 +1,31 @@
 import os
 from flask import (
     Flask,
-    abort,
+    jsonify,
     request,
 )
-from werkzeug.wrappers import Response
 
-from app.models import User
-from app.utils import profile_utils
-
-app = Flask(__name__)
+from src.app.models import User
+from src.app.utils import profile_utils
 
 
-@app.route('/profile/similarity', method=('POST',))
-def profiles_similarity():
-    data = request.get_json()
-    user = User.get_or_create(user_id=data['user_id'], text=data['user_text'])
-    reviewer = User.get_or_create(user_id=data['reviewer_id'], text=data['reviewer_text'])
-    similarity = profile_utils.calc_similarity(user.profile, reviewer.profile)
-    return Response(
-        status=200,
-        content_type='application/json',
-        response={'similarity': similarity},
-    )
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+
+    @app.route('/api/v1/profiles/similarity', methods=('POST',))
+    def profiles_similarity():
+        data = request.get_json()
+        user = User.get_or_create(user_id=data['user_id'], text=data['user_text'])
+        reviewer = User.get_or_create(user_id=data['reviewer_id'], text=data['reviewer_text'])
+        similarity = profile_utils.calc_similarity(user.profile, reviewer.profile)
+        return jsonify(similarity=similarity)
+
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(
         host='0.0.0.0',
         port=int(os.environ.get('FLASK_PORT')),
