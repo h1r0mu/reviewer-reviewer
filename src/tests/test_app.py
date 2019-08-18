e24ede6bef4e6900b53c459f1cb217d63ab03399
Hiromu Asahina <asahina@sasase.ics.keio.ac.jp>
2019-08-18 23:17:00 +0900

    Fixed tests for app to use mocked API client. #8

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,10 +4,14 @@ from unittest import (
     TestCase,
     skipIf,
 )
+from unittest.mock import patch
+from reviewer.models import APIClient
 
-from reviewer import app
+from reviewer import create_app
 
 SKIP_USER_STATE_TESTS = os.environ.get('DYNAMODB_HOST', None) is None
+PROFILE_URL = os.environ.get('PERSONALITY_INSIGHTS_URL',
+                             'https://gateway.watsonplatform.net/personality-insights/api/v3/profile')
 os.environ['DYNAMODB_HOST'] = ''
 
 
@@ -15,10 +19,10 @@ os.environ['DYNAMODB_HOST'] = ''
 class ProfilesSimilarityTest(TestCase):
 
     def setUp(self) -> None:
-        _app = app.create_app()
+        _app = create_app()
         self.client = _app.test_client()
         self.url = '/api/v1/profiles/similarity'
-        self.profile_url = 'https://gateway.watsonplatform.net/personality-insights/api/v3/profile'
+        self.profile_url = PROFILE_URL
 
     @responses.activate
     def test_profiles_similarity(self):
@@ -28,16 +32,19 @@ class ProfilesSimilarityTest(TestCase):
                       body=profile_response, status=200,
                       content_type='application/json')
 
-        response = self.client.post(
-            self.url,
-            json={
-                'user_id': '0000000000AAAAAA',
-                'user_text': 'The text might be a string of joined tweets',
-                'reviewer_id': '9999999999ZZZZZZ',
-                'reviewer_text': 'The text might be a string joined reviews',
-            },
-            headers={'Accept': 'application/json'},
-        )
+        with open(os.path.join(os.path.dirname(__file__), '../resources/personality-v3-expect1.txt')) as expect_file:
+            profile_response = expect_file.read()
+        with patch.object(APIClient, 'get_profile', return_value=profile_response) as mock_get_profile:
+            response = self.client.post(
+                self.url,
+                json={
+                    'user_id': '0000000000AAAAAA',
+                    'user_text': 'The text might be a string of joined tweets',
+                    'reviewer_id': '9999999999ZZZZZZ',
+                    'reviewer_text': 'The text might be a string joined reviews',
+                },
+                headers={'Accept': 'application/json'},
+            )
 
         expected_response = {'similarity': 1.0}
 
