a066e0ec0400893b105e0a1296fbbdf5e7e8b8f6
Hiromu Asahina <asahina@sasase.ics.keio.ac.jp>
2019-08-20 22:13:39 +0900

    Refactored JS. #41

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,38 +1,36 @@
-async function getTweets(count) {
+async function getTweets(count = 200) {
     OAuth.initialize('wgFu5bezL7NdCCK5giqQH_xDB4U');
     let result = await OAuth.popup("twitter");
     let credentials = await result.get('/1.1/account/verify_credentials.json');
     let timeline = await result.get(`/1.1/statuses/user_timeline.json?screen_name=${credentials.screen_name}&count=${count}`)
-    return {
-        credentials: credentials,
-        timeline: timeline
-    };
+    let user_id = credentials.screen_name;
+    let tweets = timeline.map(tweet => tweet.text);
+    return [user_id, tweets];
 }
 
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+
+chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
     console.log(msg);
     switch (msg.command) {
         case "getTweets":
-            console.log("Get tweets");//送られたメッセージをキャッチ
-            getTweets(200)
-                .then(tweets => {
-                    console.log(tweets)
-                    let user_id = tweets.credentials.screen_name;
-                    let user_text = tweets.timeline.map(tweet => tweet.text).join(" ");
-                    localStorage.setItem("rr_user_id", user_id);
-                    localStorage.setItem("rr_user_text", user_text);
-                });
+            let tweets = await getTweets();
+            sendResponse({tweets: tweets});
+            // chrome.tabs.sendMessage(sender.tab.id, {
+            //     type: "sendResponse",
+            //     command: "getTweest",
+            //     tweets: tweets
+            // });
             break;
-        case "getProfilesSimilarity":
-            if (!("rr_user_id" in localStorage && "rr_user_text" in localStorage)) {
-                console.error("Get tweets first.");
-                break;
-            }
-            msg.request.user_id = localStorage.getItem("rr_user_id");
-            msg.request.user_text = localStorage.getItem("rr_user_text");
-            let url = "http://localhost:8000/api/v1/profiles/similarity"
-            console.log(msg.request)
-            return axios.post(url, msg.request);
+        case "axiosPost":
+            let response = await axios.post(url, request);
+            chrome.tabs.sendMessage(sender.tab.id, {
+                type: 'sendResponse',
+                response: response
+            });
+            break;
+        default:
+            console.log(`Unknown commmand ${msg.command} is specified.`);
+            break;
     }
 });
 
--- chrome/js/content_scripts.js
+++ chrome/js/content_scripts.js
@@ -1,3 +1,89 @@
+class Message {
+    constructor(command, func) {
+        this.command = command;
+        this.func = func;
+    }
+}
+
+class Client {
+    constructor() {
+        this.url = "http://localhost:8000/api/v1/profiles/similarity"
+    }
+
+    async getProfilesSimilarity(user, reviewer) {
+        let msg = {
+            command: 'axiosPost',
+            url: this.url,
+            request: {
+                user_id: user.id,
+                user_text: user.tweets,
+                reviewer_id: reviewer.id,
+                reviewer_text: reviewer.reviews
+            }
+        };
+        return new Promise((resolve, reject) => {
+            console.log('send request');
+            chrome.runtime.sendMessage(msg);
+            chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+                if (message.type === "sendResponse" && message.command === "axiosPost") {
+                    resolve(message.response);
+                }
+            });
+        });
+    }
+}
+
+
+class User {
+    constructor(id, tweets = []) {
+        this.id = id;
+        this.rawTweets = tweets;
+    }
+
+    get tweets() {
+        return this.rawTweets.join(' ');
+    }
+}
+
+class Reviewer {
+    constructor(profileUrl, reviews = [], similarity = null) {
+        this.profileUrl = profileUrl;
+        this.rawReviews = reviews;
+        this.similarity = similarity;
+    }
+
+    get id() {
+        return this.profileUrl.split('/')[5].split('.')[2];
+    }
+
+    get reviews() {
+        return this.rawReviews.join(' ');
+    }
+}
+
+class ChromeStorageWrapper {
+
+    getUserId() {
+        return this.getProfileText('user_id');
+    }
+
+    setUserId(userId) {
+        return this.setProfileText('user_id', userId);
+    }
+
+    getProfileText(id) {
+        let value = "";
+        chrome.storage.local.get([`rr_profiles_${id}`], result => {
+            value = result[`rr_profiles_${id}`];
+        })
+        return value;
+    }
+
+    setProfileText(id, text) {
+        chrome.storage.local.set({[`rr_profiles_${id}`]: text});
+    }
+}
+
 function genPosNegReviewUrls(productUrl) {
     let domain = productUrl.split('/')[2];
     let product = productUrl.match(/([A-Z]|[0-9]){10}/)[0];
@@ -10,111 +96,89 @@ function genPosNegReviewUrls(productUrl) {
     }
 }
 
-function getDocument(reviewUrl) {
-    return axios.get(reviewUrl)
-        .then(response => {
-            let parser = new DOMParser();
-            return parser.parseFromString(response.data, "text/html");
-        })
-        .catch(error => {
-            console.log(error);
-        })
+async function getDocument(reviewUrl) {
+    let parser = new DOMParser();
+    try {
+        let response = await axios.get(reviewUrl);
+        return parser.parseFromString(response.data, "text/html");
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-function findUserUrls(reviewDocument) {
+async function getTweets(count = 200) {
+    let msg = {
+        command: 'getTweets'
+    };
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage(msg, response => {
+            console.log(response);
+            resolve(response.tweets);
+        });
+        // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        //     if (message.type === "sendResponse" && message.command === "getTweets") {
+        //         resolve(message.tweets);
+        //     }
+        // });
+    });
+}
+
+function findReviewerProfileUrls(reviewDocument) {
     // TODO: remove slice
     let users = reviewDocument.getElementsByClassName("a-profile");
-    return Object.values(users).map(user => user.href).slice(0,2);
+    return Object.values(users).map(user => user.href).slice(0, 2);
 }
 
-function findUserReviewUrls(userUrl) {
+function findReviewerReviewUrls(url) {
     return new Promise((resolve, reject) => {
         let iframe = document.createElement("iframe");
-        iframe.src = userUrl;
+        iframe.src = url;
         iframe.hidden = true;
         iframe.onload = () => {
             let userReviews = iframe
                 .contentDocument
                 .getElementsByClassName('a-link-normal profile-at-review-link a-text-normal');
-            resolve(
-                {
-                    userUrl: userUrl,
-                    reviewUrls: Object.values(userReviews).map(userReview => userReview.href)
-                }
-            );
             document.body.removeChild(iframe);
+            resolve(Object.values(userReviews).map(userReview => userReview.href));
         };
         document.body.appendChild(iframe);
     })
 }
 
-function findUsersReviewUrls(userUrls) {
-    return Promise.all(userUrls.map(findUserReviewUrls))
+async function getReviewerReviews(url) {
+    let reviewDocument = await getDocument(url);
+    let reviewBody = reviewDocument.getElementsByClassName('a-size-base review-text review-text-content');
+    return reviewBody[0].textContent;
 }
 
-function getUserReviews(user) {
-    return Promise.all(
-        user.reviewUrls.map(url => {
-            return getDocument(url)
-                .then(reviewDocument => {
-                        let reviewBody = reviewDocument
-                            .getElementsByClassName('a-size-base review-text review-text-content');
-                        return reviewBody[0].textContent;
-                    }
-                )
-        }))
-}
-
-function getUsersReviews(usersReviewUrls) {
-    return Promise.all(
-        usersReviewUrls.map(user => {
-                return getUserReviews(user)
-                    .then(reviews => {
-                            return {
-                                userUrl: user.userUrl,
-                                reviewUrls: user.reviewUrls,
-                                reviews: reviews
-                            }
-                        }
-                    )
-            }
-        )
-    )
-}
-
-async function getSimilarity(userReviews) {
-    let request = {
-        user_id: "",
-        user_text: "",
-        reviewer_id: userReviews.userUrl,
-        reviewer_text: userReviews.reviews.join(" "),
+async function sortReviewsByPersonality() {
+    let client = new Client();
+    let storage = new ChromeStorageWrapper();
+    let user_id = storage.getUserId();
+    let tweets = storage.getProfileText(user_id);
+    if ((user_id == false || tweets == false)) {
+        user_id, tweets = await getTweets();
+        storage.setUserId(user_id);
+        storage.setProfileText(user_id, tweets);
     }
-    let msg = {
-        command: "getProfilesSimilarity",
-        request: request
-    };
-    chrome.runtime.sendMessage(msg);
-    console.log('send request');
-}
-
-async function getSimilarities(usersReviews) {
-    console.log(usersReviews);
-    return await Promise.all(
-       usersReviews.map(getSimilarity)
-    )
-}
-
-
-function sortReviewsByPersonality() {
-    let reviewUrls = genPosNegReviewUrls(location.href)
+    let user = new User(user_id, tweets);
+    let reviewUrls = genPosNegReviewUrls(location.href);
     for (let reviewUrl of Object.values(reviewUrls)) {
-        getDocument(reviewUrl)
-            .then(findUserUrls)
-            .then(findUsersReviewUrls)
-            .then(getUsersReviews)
-            .then(getSimilarities)
-            .catch(error => console.log(error))
-        break;
+
+        let reviewPageDocument = await getDocument(reviewUrl);
+        let reviewerProfileUrls = await findReviewerProfileUrls(reviewPageDocument);
+        let reviewers = reviewerProfileUrls.map(url => {
+            return new Reviewer(url);
+        });
+        reviewers.map(async reviewer => {
+            let texts = storage.getProfileText(reviewer.id);
+            if (texts == "") {
+                let reviewUrls = await findReviewerReviewUrls(reviewer.profileUrl);
+                reviewer.reviews = await Promise.all(reviewUrls.map(getReviewerReviews));
+                storage.setProfileText(reviewer.id, reviewer.reviews);
+            }
+            client.getProfilesSimilarity(user, reviewer);
+        });
     }
 }
 
